92908e0cfb1c93a21aeb6f9684877fa8fe7d2abb
Greg Newkirk <gregnewkirk@gmail.com>
2026-03-26 11:03:45 -0700

    Add Google Scholar link to socials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,6 +46,15 @@ const socials = [
       </svg>
     ),
   },
+  {
+    name: "Google Scholar",
+    href: "https://scholar.google.com/citations?user=sI--g3gAAAAJ&hl=en",
+    icon: (
+      <svg viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+        <path d="M12 24a7 7 0 1 1 0-14 7 7 0 0 1 0 14zm0-24L0 9.5l4.838 3.94A8 8 0 0 1 12 9a8 8 0 0 1 7.162 4.44L24 9.5z" />
+      </svg>
+    ),
+  },
 ];
 
 const ventures = [
