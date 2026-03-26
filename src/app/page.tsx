0bd701659d52cf33e88725f5b162e5dc623a534f
Greg Newkirk <gregnewkirk@gmail.com>
2026-03-26 11:10:55 -0700

    Remove X from socials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,15 +11,6 @@ const socials = [
     ),
   },
   {
-    name: "X",
-    href: "https://x.com/drgregshow",
-    icon: (
-      <svg viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
-        <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
-      </svg>
-    ),
-  },
-  {
     name: "ORCID",
     href: "https://orcid.org/0000-0002-1154-7787",
     icon: (
